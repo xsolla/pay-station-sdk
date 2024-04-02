9ada30e2ad23a299c87d42defa0c06c81e1b956b
e.kireev <e.kireev@xsolla.com>
2024-04-02 09:35:19 +0300

    feat: add enum for next actions and event names

---
+++ src/core/actions/next-action-type.enum.ts
@@ -0,0 +1,11 @@
+export enum NextActionType {
+  checkStatus = 'check_status',
+  showFields = 'show_fields',
+  statusUpdated = 'status_updated',
+  showErrors = 'show_errors',
+  redirect = 'redirect',
+  threeDS = '3DS',
+  specialButton = 'special_button',
+  showQrCode = 'show_qr_code',
+  showMobilePaymentScreen = 'show_mobile_payment_screen',
+}
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ import './styles/style.scss';
 export { headlessCheckout } from './features/headless-checkout/headless-checkout.instance';
 export * from './web-components';
 export { Lang } from './core/i18n/lang.enum';
+export { EventName } from './core/event-name.enum';
+export { NextActionType } from './core/actions/next-action-type.enum';
