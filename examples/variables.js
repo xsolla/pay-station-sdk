47225cbb44a93c8801e94250916e25a97ad70c0e
a.kornienko <a.kornienko@xsolla.com>
2024-04-02 13:09:02 +0300

    docs(PAYMENTS-18740): update documentation

---
+++ examples/variables.js
@@ -0,0 +1,2 @@
+const payStationSdkUndefinedError = `It seems the SDK library is undefined.
+  Please, link the PayStationSdk source or create a local build (recommended for testing purposes only).`;
