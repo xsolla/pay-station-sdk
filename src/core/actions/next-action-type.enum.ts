18c2d5f22e32d0612cf069474171c3523f985099
a.kornienko <a.kornienko@xsolla.com>
2024-05-20 09:30:41 +0300

    feat(PAYMENTS-19010): add translates

--- src/core/actions/next-action-type.enum.ts
+++ src/core/actions/next-action-type.enum.ts
@@ -8,4 +8,6 @@ export enum NextActionType {
   specialButton = 'special_button',
   showQrCode = 'show_qr_code',
   showMobilePaymentScreen = 'show_mobile_payment_screen',
+  hideForm = 'hide_form',
+  showCashPaymentInstruction = 'show_cash_payment_instruction',
 }
