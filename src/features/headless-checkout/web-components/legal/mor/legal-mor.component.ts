f31f48ce98a57adc0bbebc75c4386f16b28a0483
simbirromanmakarov <roman.makarov@simbirsoft.com>
2025-03-19 10:28:35 +0400

    feat: update legal text

--- src/features/headless-checkout/web-components/legal/mor/legal-mor.component.ts
+++ src/features/headless-checkout/web-components/legal/mor/legal-mor.component.ts
@@ -1,12 +1,15 @@
-import { LegalPartComponentAbstract } from '../legal-part-component.abstract';
+import { LegalConfigPartComponentAbstract } from '../legal-config-part-component.abstract';
 import { getLegalMorComponentTemplate } from './legal-mor.component.tempate';
 
 import './legal-mor.component.scss';
 
-export class LegalMorComponent extends LegalPartComponentAbstract {
+export class LegalMorComponent extends LegalConfigPartComponentAbstract {
   protected readonly componentName = 'LegalMorComponent';
 
   protected getHtml(): string {
-    return getLegalMorComponentTemplate();
+    if (this.config) {
+      return getLegalMorComponentTemplate(this.config);
+    }
+    return '';
   }
 }
--- src/features/headless-checkout/web-components/legal/terms/legal-terms.component.tempate.ts
+++ src/features/headless-checkout/web-components/legal/terms/legal-terms.component.tempate.ts
@@ -1,8 +1,23 @@
 import i18next from 'i18next';
 import { termsUrl } from '../links.const';
+import {
+  LegalArea,
+  LegalComponentConfig,
+} from '../legal-component.config.interface';
 
-export const getLegalTermsComponentTemplate = (): string => {
-  return i18next.t('legal-terms', {
+const legalAreaLabelMap = new Map<LegalArea, string>([
+  ['EU', 'legal-terms-eu'],
+]);
+
+const getLabel = (legalArea: LegalArea = 'other'): string => {
+  return legalAreaLabelMap.get(legalArea) ?? 'legal-terms';
+};
+
+export const getLegalTermsComponentTemplate = (
+  config: LegalComponentConfig,
+): string => {
+  const label = getLabel(config.legalArea);
+  return i18next.t(label, {
     xsolla_terms_link: termsUrl,
   });
 };
