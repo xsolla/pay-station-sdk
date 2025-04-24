7d7ff213883514d87641e8f2c075feb2b0aea07f
e.kireev <e.kireev@xsolla.com>
2025-04-24 14:55:31 +0300

    feat: reinitialize sdk, deprecate destroy

--- src/features/headless-checkout/headless-checkout.instance.ts
+++ src/features/headless-checkout/headless-checkout.instance.ts
@@ -2,4 +2,27 @@ import { container } from 'tsyringe';
 import { HeadlessCheckout } from './headless-checkout';
 
 container.register<Window>(Window, { useValue: window });
-export const headlessCheckout = container.resolve(HeadlessCheckout);
+
+let currentInstance: HeadlessCheckout = container.resolve(HeadlessCheckout);
+
+export const headlessCheckout = new Proxy({} as HeadlessCheckout, {
+  get: (target, prop) => {
+    if (prop === 'destroy') {
+      return (): void => {
+        currentInstance.destroy();
+        container.clearInstances();
+        container.register<Window>(Window, { useValue: window });
+        currentInstance = container.resolve(HeadlessCheckout);
+      };
+    }
+
+    return Reflect.get(
+      currentInstance,
+      prop,
+    ) as HeadlessCheckout[keyof HeadlessCheckout];
+  },
+
+  set: (target, prop, value) => {
+    return Reflect.set(currentInstance, prop, value);
+  },
+});
