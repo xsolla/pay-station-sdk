ee803e7cb193a8f658dab5264713024acbbdbb2a
e.kireev <e.kireev@xsolla.com>
2025-07-01 13:24:24 +0300

    [PAYMENTS-23313] sdk: apple pay qr

---
+++ src/core/guards/apple-pay/apple-pay-qr-opened.guard.ts
@@ -0,0 +1,12 @@
+import { Message } from '../../message.interface';
+import { isEventMessage } from '../event-message.guard';
+import { EventName } from '../../event-name.enum';
+
+export const isApplePayQrOpenedEventMessage = (
+  messageData: unknown
+): messageData is Message<{ error: string } | null | undefined> => {
+  if (isEventMessage(messageData)) {
+    return messageData.name === EventName.applePayQrOpened;
+  }
+  return false;
+};
---
+++ src/features/headless-checkout/post-messages-handlers/apple-pay/apple-pay-qr-closed.handler.ts
@@ -0,0 +1,21 @@
+import { Handler } from '../../../../core/post-messages-client/handler.type';
+import { Message } from '../../../../core/message.interface';
+import {isApplePayQrClosedEventMessage} from '../../../../core/guards/apple-pay/apple-pay-qr-closed.guard';
+
+export const applePayQrClosedHandler: Handler<
+  { error: string } | null | undefined
+> = (
+  message: Message
+): {
+  isHandled: boolean;
+  value: { error: string } | null | undefined;
+} | null => {
+  if (!isApplePayQrClosedEventMessage(message)) {
+    return null;
+  }
+
+  return {
+    isHandled: true,
+    value: message.data,
+  };
+};
