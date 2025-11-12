671ad37d51560b7fc2004c56dc37eb04da9e7e14
e.kireev <e.kireev@xsolla.com>
2025-11-12 21:25:29 +0800

    feat(PAYMENTS-26178): feat transactionInitiationType

--- src/core/form/types/credit-card-form-configuration.interface.ts
+++ src/core/form/types/credit-card-form-configuration.interface.ts
@@ -1,33 +1,41 @@
-import { cardPid } from '../../../features/headless-checkout/web-components/payment-methods/variables';
-import { BasePaymentConfiguration } from '../form-configuration.interface';
+import {cardPid} from '../../../features/headless-checkout/web-components/payment-methods/variables';
+import {BasePaymentConfiguration} from '../form-configuration.interface';
 
 interface SpecialPaymentSettingsBase {
-  useSingleExpirationDateField?: boolean;
+    useSingleExpirationDateField?: boolean;
 }
 
 type SpecialPaymentSettings = SpecialPaymentSettingsBase & {
-  useExternalVault?: false;
-  isPaymentViaSavedMethod?: never;
+    useExternalVault?: false;
+    /**
+     * @deprecated Use transactionInitiationType instead
+     */
+    isPaymentViaSavedMethod?: never;
+    transactionInitiationType?: never;
 };
 
 type SpecialPaymentSettingsWithVault = SpecialPaymentSettingsBase & {
-  useExternalVault: true;
-  isPaymentViaSavedMethod: boolean;
+    useExternalVault: true;
+    /**
+     * @deprecated Use transactionInitiationType instead
+     */
+    isPaymentViaSavedMethod?: boolean;
+    transactionInitiationType?: 'COF' | 'OT'
 };
 
 type PaymentConfigurationCreditCardBase = BasePaymentConfiguration & {
-  paymentMethodId: typeof cardPid;
+    paymentMethodId: typeof cardPid;
 };
 
 type PaymentConfigurationCreditCard = PaymentConfigurationCreditCardBase & {
-  paymentMethodSettings: SpecialPaymentSettings;
+    paymentMethodSettings: SpecialPaymentSettings;
 };
 
 type PaymentConfigurationCreditCardWithVault =
-  PaymentConfigurationCreditCardBase & {
+    PaymentConfigurationCreditCardBase & {
     paymentMethodSettings: SpecialPaymentSettingsWithVault;
-  };
+};
 
 export type PaymentConfigurationCreditCardSettings =
-  | PaymentConfigurationCreditCard
-  | PaymentConfigurationCreditCardWithVault;
+    | PaymentConfigurationCreditCard
+    | PaymentConfigurationCreditCardWithVault;
