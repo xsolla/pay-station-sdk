b396f07ef8d6dab7cdf44e54a62095db43772312
p.zaslavsky <p.zaslavsky@xsolla.com>
2025-12-15 18:28:42 +0700

    fix(events): prevent losing Apple Pay external window close event

--- src/features/headless-checkout/web-components/payment-methods/variables.ts
+++ src/features/headless-checkout/web-components/payment-methods/variables.ts
@@ -2,5 +2,3 @@
 export const cardPid = 1380 as const;
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
 export const googlePayPid = 3431 as const;
-// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-export const applePayPid = 3175 as const;
