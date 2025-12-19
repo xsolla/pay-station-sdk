7866fd7b23236b0f60399a136e189e0577c2c346
Nikolay Poluhin <ni.poluhin@xsolla.com>
2025-12-19 12:09:56 +0500

    [PAYMENTS-26415]: worked linter

--- src/features/headless-checkout/environment.ts
+++ src/features/headless-checkout/environment.ts
@@ -1,5 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
 export const headlessCheckoutAppUrl = process.env.HUI_URL || 'https://secure.xsolla.com/headless-checkout';
-
+// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
 export const headlessCheckoutSandboxAppUrl = process.env.HUI_URL || 'https://sandbox-secure.xsolla.com/headless-checkout';
 
 export const cdnIconsUrl =
