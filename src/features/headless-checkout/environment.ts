5c000d0bf836b0d13cd908a47f159618646a4233
Nikolay Poluhin <ni.poluhin@xsolla.com>
2025-12-24 12:12:26 +0500

    [PAYMENTS-26415]: review fixes

--- src/features/headless-checkout/environment.ts
+++ src/features/headless-checkout/environment.ts
@@ -1,6 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+
 export const headlessCheckoutAppUrl = process.env.HEADLESS_UI_URL || 'https://secure.xsolla.com/headless-checkout';
-// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
 export const headlessCheckoutSandboxAppUrl = process.env.HEADLESS_UI_URL || 'https://sandbox-secure.xsolla.com/headless-checkout';
 
 export const cdnIconsUrl =
