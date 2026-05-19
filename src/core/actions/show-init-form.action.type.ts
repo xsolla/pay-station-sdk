de53be525353e471704bfc56da24760b857de1ae
e.kireev <e.kireev@xsolla.com>
2026-05-19 14:51:13 +0300

    code review

--- src/core/actions/show-init-form.action.type.ts
+++ src/core/actions/show-init-form.action.type.ts
@@ -1,10 +1,14 @@
-import { FormError } from '../form/form-error.interface';
 import { Action } from './action.interface';
+import { Field } from '../form/field.interface';
+import { FormMessage } from '../form/form-message.interface';
+import { FormError } from '../form/form-error.interface';
 
 export type ShowInitFormActionType = 'show_init_form';
 
 export interface ShowInitFormActionData {
-  errors: FormError[] | null;
+  fields: Field[];
+  errors: FormError[] | undefined;
+  messages: FormMessage[] | null;
 }
 
 export type ShowInitFormAction = Action<
