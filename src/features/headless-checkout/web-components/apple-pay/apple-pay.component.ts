1f3bb84517e135b39df70fbb08c33b879c5595e1
Roman Makarov <r.makarov@xsolla.com>
2026-06-17 15:41:16 +0400

    feat(PAYMENTS-28786): add apple pay button loader

--- src/features/headless-checkout/web-components/apple-pay/apple-pay.component.ts
+++ src/features/headless-checkout/web-components/apple-pay/apple-pay.component.ts
@@ -163,7 +163,8 @@ export class ApplePayComponent extends SecureComponentAbstract {
 
   protected getSecureHtml(): string {
     const appUrl = this.environmentService.getHeadlessCheckoutAppUrl();
-    return `<iframe id="apple-pay-iframe" src='${appUrl}/secure-components/${this
+    return `<div class="apple-button-loader"></div>
+      <iframe id="apple-pay-iframe" src='${appUrl}/secure-components/${this
       .componentName!}' allow='payment'></iframe>`;
   }
 
