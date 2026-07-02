3509189f72aee2a69a411b70833b0ffeb4ee0a60
p.zaslavsky <p.zaslavsky@xsolla.com>
2026-07-02 17:25:32 +0700

    PAYMENTS-29021: fix canceled status

--- src/features/headless-checkout/web-components/status/template-config/get-status-state.function.spec.ts
+++ src/features/headless-checkout/web-components/status/template-config/get-status-state.function.spec.ts
@@ -2,6 +2,7 @@ import { getStatusState } from './get-status-state.function';
 import { StatusEnum } from '../../../../../core/status/status.enum';
 import { Status } from '../../../../../core/status/status.interface';
 import { StatusState } from './status-state.enum';
+import { TitleClass } from '../../../../../core/status/title-class.enum';
 
 describe('getStatusState', () => {
   it('Should return processing state', () => {
@@ -23,5 +24,32 @@ describe('getStatusState', () => {
     expect(getStatusState({ statusState: StatusEnum.done } as Status)).toEqual(
       StatusState.isSuccess,
     );
+  });
+
+  it('Should return cancel state when titleClass is cancel', () => {
+    expect(
+      getStatusState({
+        statusState: StatusEnum.processing,
+        titleClass: TitleClass.cancel,
+      } as Status),
+    ).toEqual(StatusState.isCanceled);
+  });
+
+  it('Should return cancel state when titleClass is cancelUser', () => {
+    expect(
+      getStatusState({
+        statusState: StatusEnum.processing,
+        titleClass: TitleClass.cancelUser,
+      } as Status),
+    ).toEqual(StatusState.isCanceled);
+  });
+
+  it('Should return cancel state when isCancelUser is true', () => {
+    expect(
+      getStatusState({
+        statusState: StatusEnum.processing,
+        isCancelUser: true,
+      } as Status),
+    ).toEqual(StatusState.isCanceled);
   });
 });
